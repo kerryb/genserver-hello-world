a9324996d9da63c9992f52b6a75447df0a9df50f
Kerry Buckley <kerryjbuckley@gmail.com>
2017-08-03 20:33:18 +0100

    Reinitialise with --sup
    $ cd ..
    $ mix new --sup hello_world
    The directory "hello_world" already exists. Are you sure you want to continue? [Yn]
    * creating README.md
    README.md already exists, overwrite? [Yn] y
    * creating .gitignore
    .gitignore already exists, overwrite? [Yn] y
    * creating mix.exs
    mix.exs already exists, overwrite? [Yn] y
    * creating config
    * creating config/config.exs
    config/config.exs already exists, overwrite? [Yn] y
    * creating lib
    * creating lib/hello_world.ex
    lib/hello_world.ex already exists, overwrite? [Yn] n
    * creating lib/hello_world/application.ex
    * creating test
    * creating test/test_helper.exs
    test/test_helper.exs already exists, overwrite? [Yn] y
    * creating test/hello_world_test.exs
    test/hello_world_test.exs already exists, overwrite? [Yn] y
    
    Your Mix project was created successfully.
    You can use "mix" to compile it, test it, and more:
    
        cd hello_world
        mix test
    
    Run "mix help" for more commands.
    $ cd -

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule HelloWorld.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {HelloWorld.Application, []}
     ]
   end
 
