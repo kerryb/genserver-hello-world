7aea99b6f48f9fb7e3a3b90135f04e3adc2017e6
Kerry Buckley <kerryjbuckley@gmail.com>
2017-08-03 20:26:31 +0100

    Add start_link and next to counter, with name
    iex(8)> HelloWorld.Counter.start_link {:counter_1, 1}
    {:ok, #PID<0.240.0>}
    iex(9)> HelloWorld.Counter.start_link {:counter_2, 10}
    {:ok, #PID<0.242.0>}
    iex(10)> HelloWorld.Counter.next :counter_1
    1
    iex(11)> HelloWorld.Counter.next :counter_1
    2
    iex(12)> HelloWorld.Counter.next :counter_2
    10
    iex(13)> HelloWorld.Counter.next :counter_1
    3
    iex(14)> HelloWorld.Counter.next :counter_2
    11

--- lib/hello_world/counter.ex
+++ lib/hello_world/counter.ex
@@ -1,6 +1,14 @@
 defmodule  HelloWorld.Counter do
   use GenServer
 
+  def start_link({name, start_at}) do
+    GenServer.start_link __MODULE__, start_at, name: name
+  end
+
+  def next(name) do
+    GenServer.call name, :next
+  end
+
   def handle_call(:next, _from, count) do
     {:reply, count, count + 1}
   end
