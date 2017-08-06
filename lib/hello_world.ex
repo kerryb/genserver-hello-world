fbbebb77f9e88f87d6a10c441b5f11d8235f47ae
Kerry Buckley <kerryjbuckley@gmail.com>
2017-08-06 17:55:45 +0100

    Run hello as an async task
    $ iex -S mix
    Erlang/OTP 20 [erts-9.0] [source] [64-bit] [smp:4:4] [ds:4:4:10] [async-threads:10] [hipe] [kernel-poll:false] [dtrace]
    
    TICK
    TOCK
    Interactive Elixir (1.5.0) - press Ctrl+C to exit (type h() ENTER for help)
    iex(1)> Hello World 1
    TICK
    TOCK
    Hello World 2
    TICK
    TOCK
    Hello World 3
    TICK
    TOCK
    TICK
    TOCK
    Hello World 5
    Hello World 4

--- lib/hello_world.ex
+++ lib/hello_world.ex
@@ -1,5 +1,18 @@
 defmodule HelloWorld do
+  use GenServer
+
   def hello(n) do
+    GenServer.start HelloWorld, n
+  end
+
+  def init(n) do
+    GenServer.cast self(), :hello
+    {:ok, n}
+  end
+
+  def handle_cast(:hello, n) do
+    :rand.uniform * 1900 |> round |> Process.sleep
     IO.puts "Hello World #{n}"
+    {:stop, :normal, n}
   end
 end
